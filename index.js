f607a8ff3262ff9bee49dc5f8b13f4b4f0aaa87e
CanhHacker <toicanh25@gmail.com>
2020-07-11 09:57:17 +0700

    Use lowdb

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var port = 3000
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+
+db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write();
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -11,23 +25,34 @@ app.get('/', (req, res) => res.render('index',{
 
 var  users = [
     {id: 1, name: 'Canh'},
-    {id: 2, name: 'Anh'}
+    {id: 2, name: 'Anh'},
+    {id: 3, name: 'Nhu'}
 ]
 
 app.get('/users', (req, res) => res.render('users/index',{
-   users : users
+   users : db.get('users').value()
 }))
 
 
 
-app.get('/users/search',function(req, res){
+app.get('/users/search', function(req, res) {
     var q = req.query.q;
-    var MatchedUsers = users.filter(function(users){
-        return users.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    })
-    res.render('users/index',{
-        users : MatchedUsers
-     })
+    var matchedUsers = users.filter(function(user) {
+      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+  
+    res.render('users/index', {
+      users: matchedUsers
+    });
+  });
+
+app.get('/users/create', function(req, res){
+    res.render('users/create');
+})
+
+app.post('/users/create',function(req, res){
+    db.get('users').push(req.body).write();
+    res. redirect('/users')
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
