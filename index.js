bdb8b953c221b934e0a5d6e42e47b2291e375bc0
CanhHacker <toicanh25@gmail.com>
2020-07-12 10:34:40 +0700

    controller nha

--- index.js
+++ index.js
@@ -1,58 +1,24 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var port = 3000
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
+var express = require('express');
+var bodyParser = require('body-parser');
+var userRoute = require('./routes/user.route');
 
-db = low(adapter);
+var port = 3000;
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write();
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-
-app.set('views', './views')
-app.set('view engine', 'pug')
-
-app.get('/', (req, res) => res.render('index',{
-    name: 'Canh'
-}))
-
-var  users = [
-    {id: 1, name: 'Canh'},
-    {id: 2, name: 'Anh'},
-    {id: 3, name: 'Nhu'}
-]
-
-app.get('/users', (req, res) => res.render('users/index',{
-   users : db.get('users').value()
-}))
-
-
-
-app.get('/users/search', function(req, res) {
-    var q = req.query.q;
-    var matchedUsers = users.filter(function(user) {
-      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-  
-    res.render('users/index', {
-      users: matchedUsers
-    });
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.get('/', function(req, res) {
+  res.render('index', {
+    name: 'AAA'
   });
+});
 
-app.get('/users/create', function(req, res){
-    res.render('users/create');
-})
 
-app.post('/users/create',function(req, res){
-    db.get('users').push(req.body).write();
-    res. redirect('/users')
-})
+app.use('/users', userRoute);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, function() {
+  console.log('Server listening on port ' + port);
+});
